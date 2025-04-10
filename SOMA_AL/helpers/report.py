aa3474eb2ffb5441742f1372f8ab7a51b17755b7
chadcwilliams <cwilliams.uca@gmail.com>
2025-04-10 11:28:54 -0700

    Added participant pain exclusions

--- SOMA_AL/helpers/report.py
+++ SOMA_AL/helpers/report.py
@@ -7,7 +7,7 @@ class Report(ReportFunctions):
         super().__init__()
 
     #Report builders
-    def build_report(self, rscripts_path=None, load_stats=False) -> None:
+    def build_report(self, rscripts_path=None, load_stats=False, load_models=True) -> None:
 
         """
         Builds and prints the SOMA report
@@ -20,7 +20,8 @@ class Report(ReportFunctions):
 
         #Initiate processes
         self.print_plots()
-        self.load_modelling_results(rscripts_path, load_stats)
+        if load_models:
+            self.load_modelling_results(rscripts_path, load_stats)
 
         self.pdf = MarkdownPdf(toc_level=3)
 
@@ -78,80 +79,81 @@ class Report(ReportFunctions):
         self.add_data_pdf(section_text, center=True)
 
         #Modelling Results
-        section_text = []
-        section_text.append('## Modelling Evaluations')
-        section_text.append('### Model Fits')
-        section_text.extend(self.insert_table(self.model_AIC, 'model-AIC'))
-        section_text.extend(self.insert_table(self.model_BIC, 'model-BIC'))
-        section_text.extend(self.insert_table(self.model_AIC_percentages, 'model-AIC-percentages'))
-        section_text.extend(self.insert_table(self.model_BIC_percentages, 'model-BIC-percentages'))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append('### Number of Runs')
-        filename = 'SOMA_AL/modelling/fit-by-runs.png'
-        section_text.extend(self.insert_image('fit-by-runs', filename))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append('### Parameter Recovery')
-        for model_name in self.model_names:
-            filename = f'SOMA_AL/modelling/correlations/{model_name}_correlation_plot.png'
-            section_text.extend(self.insert_image(f'{model_name}-correlation-plot', filename))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append('### Model Recovery')
-        filename = f'SOMA_AL/modelling/model_recovery.png'
-        section_text.extend(self.insert_image('model-recovery', filename))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        best_model_name = self.best_model.split("+")[0].replace('Hybrid2','Hybrid 2').title()
-        section_text.append(f'## Modelling Results: {best_model_name}')
-        model_introduction = f"""The {best_model_name} model was selected as the best fitting model based on the BIC values.
-        This model was able to accurately recover the parameters of the simulated data and was able to recover the model structure from the simulated data.
-        The model was able to accurately predict the learning and transfer behaviours of the participants in the study.
-        Thus, the following will only describe the results of the {best_model_name} model."""
-        section_text.append(model_introduction)
-        section_text.append(f'### Model Fit')
-        filename = f'SOMA_AL/modelling/model_behaviours/{self.best_model}_model_behaviours.png'
-        section_text.extend(self.insert_image('model-behaviour', filename))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append(f'### Model Accuracy')
-        section_text.extend(self.get_statistics('learning-model-behaviour-accuracy'))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append(f'### Model Choice Rate')
-        section_text.extend(self.get_statistics('transfer-model-behaviour-choice-rate'))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.append('### Model Parameters')
-        filename = f'SOMA_AL/modelling/parameter_fits/{self.best_model}-model-fits.png'
-        section_text.extend(self.insert_image('model-parameters', filename))
-        self.add_data_pdf(section_text, center=True)
-
-        section_text = []
-        section_text.extend(self.get_statistics('model-parameters'))
-        self.add_data_pdf(section_text, center=True)
-       
-        for group in self.group_labels:
+        if load_models:
             section_text = []
-            section_text.append(f'### Model Parameter Correlations with Pain for {group.title()} Group')
-            filename = f'SOMA_AL/plots/model_parameter_by_pain_{group.replace(" ","_")}.png'
-            section_text.extend(self.insert_image(f'parameter-correlation-{group.replace(" ","-")}', filename))
+            section_text.append('## Modelling Evaluations')
+            section_text.append('### Model Fits')
+            section_text.extend(self.insert_table(self.model_AIC, 'model-AIC'))
+            section_text.extend(self.insert_table(self.model_BIC, 'model-BIC'))
+            section_text.extend(self.insert_table(self.model_AIC_percentages, 'model-AIC-percentages'))
+            section_text.extend(self.insert_table(self.model_BIC_percentages, 'model-BIC-percentages'))
             self.add_data_pdf(section_text, center=True)
 
-        section_text = []
-        section_text.extend(self.insert_table(self.model_parameters_pain['model_summary'], 'model-parameters-correlation-table'))
-        section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['no pain'], 'model-parameters-correlation-table-no'))
-        section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['acute pain'], 'model-parameters-correlation-table-acute'))
-        section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['chronic pain'], 'model-parameters-correlation-table-chronic'))
-        self.add_data_pdf(section_text, center=True)
+            section_text = []
+            section_text.append('### Number of Runs')
+            filename = 'SOMA_AL/modelling/fit-by-runs.png'
+            section_text.extend(self.insert_image('fit-by-runs', filename))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.append('### Parameter Recovery')
+            for model_name in self.model_names:
+                filename = f'SOMA_AL/modelling/correlations/{model_name}_correlation_plot.png'
+                section_text.extend(self.insert_image(f'{model_name}-correlation-plot', filename))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.append('### Model Recovery')
+            filename = f'SOMA_AL/modelling/model_recovery.png'
+            section_text.extend(self.insert_image('model-recovery', filename))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            best_model_name = self.best_model.split("+")[0].replace('Hybrid2','Hybrid 2').title()
+            section_text.append(f'## Modelling Results: {best_model_name}')
+            model_introduction = f"""The {best_model_name} model was selected as the best fitting model based on the BIC values.
+            This model was able to accurately recover the parameters of the simulated data and was able to recover the model structure from the simulated data.
+            The model was able to accurately predict the learning and transfer behaviours of the participants in the study.
+            Thus, the following will only describe the results of the {best_model_name} model."""
+            section_text.append(model_introduction)
+            section_text.append(f'### Model Fit')
+            filename = f'SOMA_AL/modelling/model_behaviours/{self.best_model}_model_behaviours.png'
+            section_text.extend(self.insert_image('model-behaviour', filename))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.append(f'### Model Accuracy')
+            section_text.extend(self.get_statistics('learning-model-behaviour-accuracy'))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.append(f'### Model Choice Rate')
+            section_text.extend(self.get_statistics('transfer-model-behaviour-choice-rate'))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.append('### Model Parameters')
+            filename = f'SOMA_AL/modelling/parameter_fits/{self.best_model}-model-fits.png'
+            section_text.extend(self.insert_image('model-parameters', filename))
+            self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.extend(self.get_statistics('model-parameters'))
+            self.add_data_pdf(section_text, center=True)
+        
+            for group in self.group_labels:
+                section_text = []
+                section_text.append(f'### Model Parameter Correlations with Pain for {group.title()} Group')
+                filename = f'SOMA_AL/plots/model_parameter_by_pain_{group.replace(" ","_")}.png'
+                section_text.extend(self.insert_image(f'parameter-correlation-{group.replace(" ","-")}', filename))
+                self.add_data_pdf(section_text, center=True)
+
+            section_text = []
+            section_text.extend(self.insert_table(self.model_parameters_pain['model_summary'], 'model-parameters-correlation-table'))
+            section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['no pain'], 'model-parameters-correlation-table-no'))
+            section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['acute pain'], 'model-parameters-correlation-table-acute'))
+            section_text.extend(self.insert_table(self.model_parameters_pain['group_summary']['chronic pain'], 'model-parameters-correlation-table-chronic'))
+            self.add_data_pdf(section_text, center=True)
         
         #Post-hocs
         if self.hide_posthocs == False:
@@ -242,15 +244,16 @@ class Report(ReportFunctions):
             section_text.extend(self.insert_table(self.transfer_rt_posthoc_interaction, 'transfer_rt_interaction', max_rows))
             self.add_data_pdf(section_text, center=True)
 
-            section_text = []
-            section_text.append('## Post-Hoc Comparisons: Model Parameters')
-            model_posthocs = self.model_parameters_posthoc_group[self.model_parameters_posthoc_group['model'] == self.best_model]
-            for parameter in model_posthocs['parameter'].unique():
-                section_text.append(f'### {parameter.replace("_"," ").replace("lr","learning rate").title()}')
-                parameter_results = model_posthocs[model_posthocs['parameter'] == parameter][['factor', 'meandiff', 'p-adj', 'reject']]
-                parameter_results.set_index('factor', inplace=True)
-                section_text.extend(self.insert_table(parameter_results, f'{parameter}-model-parameter-posthoc'))
-            self.add_data_pdf(section_text, center=True)
+            if load_models:
+                section_text = []
+                section_text.append('## Post-Hoc Comparisons: Model Parameters')
+                model_posthocs = self.model_parameters_posthoc_group[self.model_parameters_posthoc_group['model'] == self.best_model]
+                for parameter in model_posthocs['parameter'].unique():
+                    section_text.append(f'### {parameter.replace("_"," ").replace("lr","learning rate").title()}')
+                    parameter_results = model_posthocs[model_posthocs['parameter'] == parameter][['factor', 'meandiff', 'p-adj', 'reject']]
+                    parameter_results.set_index('factor', inplace=True)
+                    section_text.extend(self.insert_table(parameter_results, f'{parameter}-model-parameter-posthoc'))
+                self.add_data_pdf(section_text, center=True)
             
         #Save to pdf
         self.save_report()
